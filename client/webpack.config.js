74926fbc8be3672275740835e4d05d5f6c65a39d
katrod <katrod4545@gmail.com>
2023-07-26 09:20:58 -0600

    removed bower components

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /node_modules|bower_components/,         
+          exclude: /node_modules/,         
           use: {
             loader: 'babel-loader',
             options: {
